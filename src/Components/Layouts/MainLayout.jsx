1ab61c2e18fa30f10e7eaace93ea20afcf3c8793
tahmidhasantanoy <tahmid15-12920@diu.edu.bd>
2024-06-03 11:25:39 +0600

    Basic layout added

--- src/Components/Layouts/MainLayout.jsx
+++ src/Components/Layouts/MainLayout.jsx
@@ -1,14 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Header from "../../pages/Shared/Header";
 import Footer from "../../pages/Shared/Footer";
+import Container from "../ui/Container";
 
 const MainLayout = () => {
   return (
-    <>
+    <Container>
       <Header />
       <Outlet />
       <Footer />
-    </>
+    </Container>
   );
 };
 
