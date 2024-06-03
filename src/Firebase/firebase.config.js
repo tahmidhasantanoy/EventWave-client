bc79efc77869dae4b35fede069eedee57181f841
tahmidhasantanoy <tahmid15-12920@diu.edu.bd>
2024-06-03 11:13:51 +0600

    tailwind layout firebase added

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCZviK4QOFh0bTZfeRyMhaO5XozH1NSkpQ",
+  authDomain: "eventwave-4d8f8.firebaseapp.com",
+  projectId: "eventwave-4d8f8",
+  storageBucket: "eventwave-4d8f8.appspot.com",
+  messagingSenderId: "174421445750",
+  appId: "1:174421445750:web:b4737337d168dde209a357",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
