a7cba81015b8c10c87656c17801bc4ed8428172a
tahmidhasantanoy <tahmid15-12920@diu.edu.bd>
2024-07-12 10:11:23 +0600

    animation added

--- src/pages/Event/SingleEvent.jsx
+++ src/pages/Event/SingleEvent.jsx
@@ -7,7 +7,7 @@ const SingleEvent = ({ event }) => {
 
   const { _id, organizerName, eventDateTime, eventName, eventLocation } = event;
   return (
-    <div className="card w-full md:w-96 bg-base-100 shadow-xl m-4 group hover:bg-base-200">
+    <div className="card w-full .md:w-96 bg-base-100 shadow-xl m-4 group hover:bg-base-200">
       
       <figure className="px-10 pt-10">
         <img
--- src/pages/Home/HeroSection.jsx
+++ src/pages/Home/HeroSection.jsx
@@ -1,41 +1,97 @@
 const heroImage = "https://i.ibb.co/9gCWpHg/hero.jpg";
+import { motion } from "framer-motion";
+
 const HeroSection = () => {
+  
+  const fadeupAnimation = {
+    hide: { opacity: 0, y: 100 },
+    show: {
+      opacity: 2,
+      y: 0,
+      transition: {
+        staggerChildren: 0.4,
+        duration: 0.7,
+      },
+    },
+  };
+
   return (
     <div className="bg-gradient-to-r from-blue-50 to-white py-16 px-4">
       <div className="max-w-7xl mx-auto flex flex-col lg:flex-row items-center">
         {/* Left Section */}
-        <div className="lg:w-1/2 mb-8 lg:mb-0 text-center lg:text-left">
-          <h1 className="text-4xl lg:text-5xl font-bold mb-6">
+        <motion.div
+          variants={fadeupAnimation}
+          initial="hide"
+          animate="show"
+          className="lg:w-1/2 mb-8 lg:mb-0 text-center lg:text-left"
+        >
+          <motion.h1
+            variants={fadeupAnimation}
+            // initial="hide"
+            // animate="show"
+            className="text-4xl lg:text-5xl font-bold mb-6"
+          >
             A better way to manage your events
-          </h1>
-          <ul className="list-none space-y-2 mb-8 text-lg text-gray-700">
-            <li className="flex items-center">
+          </motion.h1>
+          <motion.ul
+            variants={fadeupAnimation}
+            // initial="hide"
+            // animate="show"
+            className="list-none space-y-2 mb-8 text-lg text-gray-700"
+          >
+            <motion.li
+              variants={fadeupAnimation}
+              // initial="hide"
+              // animate="show"
+              className="flex items-center"
+            >
               <span className="text-blue-500 mr-2">✓</span>
               Find hotels and venues
-            </li>
-            <li className="flex items-center">
+            </motion.li>
+            <motion.li
+              variants={fadeupAnimation}
+              // initial="hide"
+              // animate="show"
+              className="flex items-center"
+            >
               <span className="text-blue-500 mr-2">✓</span>
               Plan and promote your event
-            </li>
-            <li className="flex items-center">
+            </motion.li>
+            <motion.li
+              variants={fadeupAnimation}
+              // initial="hide"
+              // animate="show"
+              className="flex items-center"
+            >
               <span className="text-blue-500 mr-2">✓</span>
               Engage your attendees
-            </li>
-            <li className="flex items-center">
+            </motion.li>
+            <motion.li
+              variants={fadeupAnimation}
+              // initial="hide"
+              // animate="show"
+              className="flex items-center"
+            >
               <span className="text-blue-500 mr-2">✓</span>
               Capture leads and track ROI
-            </li>
-          </ul>
+            </motion.li>
+          </motion.ul>
           <div className="flex justify-center lg:justify-start space-x-4">
-            <button className="btn btn-primary">Explore the platform</button>
+            <motion.button
+              variants={fadeupAnimation}
+              // initial="hide"
+              // animate="show"
+              className="btn btn-primary"
+            >
+              Explore the platform
+            </motion.button>
           </div>
-        </div>
+        </motion.div>
 
         {/* Right Section */}
         <div className="flex justify-center items-center h-screen">
           <div className="relative group">
             <img
-              // src="../../../public/images/hero.jpg"
               src={heroImage}
               alt="Fancy"
               className="w-96 h-96 object-cover border-8 border-blue-500 shadow-lg transform transition-transform duration-500 group-hover:scale-105"
